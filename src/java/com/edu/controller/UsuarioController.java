20d05cc5c238bbb1e9fe374f2721d210927d91cf
Jose Pablo Ramirez Mayorga <jota_ramirezm@outlook.com>
2019-08-19 15:50:06 -0600

    Fixed Password

--- src/java/com/edu/controller/UsuarioController.java
+++ src/java/com/edu/controller/UsuarioController.java
@@ -15,9 +15,9 @@ import javax.servlet.http.HttpServletRequest;
 @ManagedBean(name = "usuarioController")
 @SessionScoped
 public class UsuarioController implements Serializable {
-
+    
     private static final long serialVersionUID = 1L;
-
+    
     SeguridadUsuario su = new SeguridadUsuario();
 
     //Variables for login
@@ -37,16 +37,16 @@ public class UsuarioController implements Serializable {
     private String codigoAcc;
     private String codigoPass;
     private String newPass;
-
+    
     public UsuarioController() {
     }
-
+    
     public String login() {
         currentUser = su.loginClient(this.user, this.password);
         System.out.println(currentUser.getRol().getNombre());
         return "galery";
     }
-
+    
     public String signup() {
         su.signUp(this.nombre, this.apellido, this.correo, this.username, this.pass);
         newUser = su.loginClient(this.username, this.pass);
@@ -54,111 +54,115 @@ public class UsuarioController implements Serializable {
         return "galery";
         
     }
-
+    
     public void verifyAccount() {
         su.verifyAccount(this.correo, this.codigoAcc);
     }
-
+    
     public void verifyPassword() {
         su.verifyPass(this.correo, this.codigoPass, this.newPass);
     }
-
+    
+    public void sendEmail() {
+        su.sendEmail(this.correo);
+    }
+    
     public String getUser() {
         return user;
     }
-
+    
     public void setUser(String user) {
         this.user = user;
     }
-
+    
     public String getPassword() {
         return password;
     }
-
+    
     public void setPassword(String password) {
         this.password = password;
     }
-
+    
     public String getNombre() {
         return nombre;
     }
-
+    
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    
     public String getApellido() {
         return apellido;
     }
-
+    
     public void setApellido(String apellido) {
         this.apellido = apellido;
     }
-
+    
     public String getCorreo() {
         return correo;
     }
-
+    
     public void setCorreo(String correo) {
         this.correo = correo;
     }
-
+    
     public String getUsername() {
         return username;
     }
-
+    
     public void setUsername(String username) {
         this.username = username;
     }
-
+    
     public String getPass() {
         return pass;
     }
-
+    
     public void setPass(String pass) {
         this.pass = pass;
     }
-
+    
     public String getCodigoAcc() {
         return codigoAcc;
     }
-
+    
     public void setCodigoAcc(String codigoAcc) {
         this.codigoAcc = codigoAcc;
     }
-
+    
     public String getCodigoPass() {
         return codigoPass;
     }
-
+    
     public void setCodigoPass(String codigoPass) {
         this.codigoPass = codigoPass;
     }
-
+    
     public String getNewPass() {
         return newPass;
     }
-
+    
     public void setNewPass(String newPass) {
         this.newPass = newPass;
     }
-
+    
     public Usuario getCurrentUser() {
         return currentUser;
     }
-
+    
     public void setCurrentUser(Usuario currentUser) {
         this.currentUser = currentUser;
     }
-
+    
     public Usuario getNewUser() {
         return newUser;
     }
-
+    
     public void setNewUser(Usuario newUser) {
         this.newUser = newUser;
     }
-
+    
     public void redirect(String url) {
         try {
             HttpServletRequest request = (HttpServletRequest) FacesContext
@@ -173,5 +177,5 @@ public class UsuarioController implements Serializable {
             e.printStackTrace();
         }
     }
-
+    
 }
